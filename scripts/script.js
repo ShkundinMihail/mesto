d2e9020ee6d223c14fb2ae95382005de742faea7
SkhundinMichail <113530482+SkhundinMichail@users.noreply.github.com>
2022-12-03 13:48:59 +0300

    fixBug3.12.22

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,16 @@
-const initialCards = [
-    {
-      name: 'MetallicA',
-      link: 'https://www.fonstola.ru/images/202009/fonstola.ru_407234.jpg'
-    },
-    {
-      name: 'MegaDeatH',
-      link: 'https://www.sunhome.ru/i/wallpapers/36/gruppa-megadeth.orig.jpg'
-    },
-    {
-      name: 'SlayeR',
-      link: 'https://wallup.net/wp-content/uploads/2019/09/426846-slayer-death-metal-heavy-thrash.jpg'
-    },
-    {
-      name: 'AntraX',
-      link: 'https://i.yapx.cc/IIkAIl.jpg'
-    },
-    {
-      name: 'TestamenT',
-      link: 'https://i.pinimg.com/originals/3a/2a/60/3a2a60546d4f3bc46d9254d2b4cc5a9e.jpg'
-    },
-    {
-      name: 'ExoduS',
-      link: 'https://i.pinimg.com/originals/7d/0d/05/7d0d05cd1fcb7561f90cff8a83b79e68.jpg'
-    }
-  ]; 
+import initialCards from './cards.js';
 
 const nameAuthor = document.querySelector('.author__name');
 const workAuthor = document.querySelector('.author__work');
-const popupOpenButtonElement = document.querySelector('.author__edit');
-const popupElementEditAuthor = document.querySelector('#edit-profile');
-const popupElementAddphoto = document.querySelector('#add-card');
-const popupCloseButtonElement = document.querySelector('#close-edit-author');
-const closePopupButtonAddPhoto = document.querySelector('#close-add-photo');
-const closePopupPhoto = document.querySelector('#close-photo-popup')
-const popupForm = document.querySelector('.popup__author-edit');
+const editAuthorButton = document.querySelector('.author__edit');
+const popupEditAuthor = document.querySelector('#edit-profile');
+const popupElementAddPhoto = document.querySelector('#add-card');
+const closeButtonEditAuthor = document.querySelector('#close-edit-author');
+const closeButtonAddPhoto = document.querySelector('#close-add-photo');
+const closeButtonPhoto = document.querySelector('#close-photo-popup')
+const popupFormAuthor = document.querySelector('.popup__author-edit');
 const inputNameAuthor = document.querySelector('.popup__input-style_edit_name');
-const inputWorkAuthor = popupForm.querySelector('.popup__input-style_edit_work');
+const inputWorkAuthor = popupFormAuthor.querySelector('.popup__input-style_edit_work');
 const elements = document.querySelector('.elements');
 const elementTemplate = document.querySelector('#element-template').content.querySelector('.element');
 const buttonAddCard = document.querySelector('.profile__add-photo');
@@ -46,7 +21,27 @@ const popupPhotoForm = document.querySelector('#popupPhotoForm');
 const titlePhotoValue = document.querySelector('#titlePhoto');
 const linkPhotoValue = document.querySelector('#linkPhoto');
 
-function elementAdd(item) {
+//Закрытие попапов
+const closePopup = function (item, mod) {
+    item.classList.remove(mod);
+};
+const closePopupEditAuthor = function () { closePopup(popupEditAuthor, 'popup_opened') };
+const closePopupAddPhoto = function () { closePopup(popupElementAddPhoto, 'popup_opened') };
+const closePopupPhoto = function () { closePopup(openPhoto, 'popup_opened-photo') };
+//Открытие попапов
+const openPopup = function (item, mod) {
+    item.classList.add(mod);
+};
+const openPopupEditAuthor = function () {
+    openPopup(popupEditAuthor,'popup_opened');
+    inputNameAuthor.value = nameAuthor.textContent;
+    inputWorkAuthor.value = workAuthor.textContent;
+};
+
+const openPopupAddPhoto = function () { openPopup(popupElementAddPhoto,'popup_opened'); };
+
+//Добавление карточек из массива
+function createElement(item) {
     const card = elementTemplate.cloneNode(true);
     const cardTitle = card.querySelector('.element__title');
     const cardPhoto = card.querySelector('.element__photo');
@@ -54,9 +49,10 @@ function elementAdd(item) {
     const photoButtonDelete = card.querySelector('#deletePhoto');
     cardTitle.textContent = item.name;
     cardPhoto.src = item.link;
+    cardPhoto.alt = item.alt;
 
     const openCard = function () {
-        openPhoto.classList.add('popup_opened');
+        openPopup(openPhoto,'popup_opened-photo');
         openTitle.textContent = cardTitle.textContent;
         openCardPhoto.src = cardPhoto.src;
     };
@@ -70,77 +66,56 @@ function elementAdd(item) {
         event.target.closest('.element').remove();
     };
     photoButtonDelete.addEventListener('click', deletePhoto);
-    return (card);
+    return card;
 };
 
 initialCards.forEach(function (item) {
-    const elementActive = elementAdd(item);
-    elements.append(elementActive);
+    const newElement = createElement(item);
+    elements.append(newElement);
 
 });
-
-const photoAdd = function (event) {
+//Добавление карточек из попапа
+const addPhoto = function (event) {
     event.preventDefault();
-    if (titlePhotoValue.value !== '' && linkPhotoValue.value !== '') {
-        const popupArray = {
-            name: titlePhotoValue.value,
-            link: linkPhotoValue.value
-        };
-        const addElementFromPopup = elementAdd(popupArray);
-        elements.prepend(addElementFromPopup);
-
-        closePopup();
-    }
-    else {
-        alert('Введете название и ссылку 👿');
-        closePopup();
+    const cardData = {
+        name: titlePhotoValue.value,
+        link: linkPhotoValue.value,
+        alt: titlePhotoValue.value
     };
-    titlePhotoValue.value = '';
-    linkPhotoValue.value = '';
-};
+    const newCard = createElement(cardData);
+    elements.prepend(newCard);
 
-const openPopupEditAuthor = function () {
-    popupElementEditAuthor.classList.add('popup_opened');
-    inputNameAuthor.value = nameAuthor.textContent;
-    inputWorkAuthor.value = workAuthor.textContent; 
-};
-const openPopupAddPhoto = function () {
-    popupElementAddphoto.classList.add('popup_opened');
-}
-const closePopup = function () {
-    popupElementEditAuthor.classList.remove('popup_opened');
-    popupElementAddphoto.classList.remove('popup_opened');
-    openPhoto.classList.remove('popup_opened');
-};
+    closePopupPhoto();
 
-const closePopupByClickOnOverlay = function (event) {
-    if (event.target !== event.currentTarget) { return; }
-    else { closePopup(); }
+    popupPhotoForm.reset();
 };
-
+//Закрытие по оверлею
+const closePopupByClickOnOverlay = event => {
+    if (event.target === event.currentTarget) {
+    closePopupEditAuthor(event.currentTarget);
+    closePopupAddPhoto(event.currentTarget);
+    closePopupPhoto(event.currentTarget);
+    }
+    }
+//Добавляет те же данные что на отображаются на странице в попап редактирования автора
 const editAuthorInfo = function () {
     nameAuthor.textContent = inputNameAuthor.value;
     workAuthor.textContent = inputWorkAuthor.value;
 };
-
-const savePopupInformation = function (evt) {
+//функция сохранения данных автора
+const saveFormInformationAuthor = function (evt) {
     evt.preventDefault();
     editAuthorInfo();
-    closePopup();
+    closePopupEditAuthor();
 };
-
-popupForm.addEventListener('submit', savePopupInformation);
-popupOpenButtonElement.addEventListener('click', openPopupEditAuthor);
-popupCloseButtonElement.addEventListener('click', closePopup);
-closePopupButtonAddPhoto.addEventListener('click', closePopup);
-closePopupPhoto.addEventListener('click', closePopup);
-popupElementEditAuthor.addEventListener('click', closePopupByClickOnOverlay);
-popupElementAddphoto.addEventListener('click', closePopupByClickOnOverlay);
+//обработчики
+popupFormAuthor.addEventListener('submit', saveFormInformationAuthor);
+editAuthorButton.addEventListener('click', openPopupEditAuthor);
 buttonAddCard.addEventListener('click', openPopupAddPhoto);
-popupPhotoForm.addEventListener('submit', photoAdd);
-
-
-
-
-
-
+closeButtonEditAuthor.addEventListener('click', closePopupEditAuthor);
+closeButtonAddPhoto.addEventListener('click', closePopupAddPhoto);
+closeButtonPhoto.addEventListener('click', closePopupPhoto);
+popupEditAuthor.addEventListener('click', closePopupByClickOnOverlay);
+popupElementAddPhoto.addEventListener('click', closePopupByClickOnOverlay);
+openPhoto.addEventListener('click', closePopupByClickOnOverlay);
+popupPhotoForm.addEventListener('submit', addPhoto);
